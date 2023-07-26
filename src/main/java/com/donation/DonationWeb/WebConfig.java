4f249e7f7defe4531dc5986fb5aebfbd20d7b3c4
whdcks2252 <whdcks4569@naver.com>
2023-07-26 20:09:47 +0900

    필터 빈으로 수정 ,CheckUserAccessInterceptor수정

--- src/main/java/com/donation/DonationWeb/WebConfig.java
+++ src/main/java/com/donation/DonationWeb/WebConfig.java
@@ -1,15 +1,21 @@
 package com.donation.DonationWeb;
 
 import com.donation.DonationWeb.argumentresolver.LoginMemberIdArgumentResolver;
+import com.donation.DonationWeb.filter.CorsFilter;
 import com.donation.DonationWeb.interceptor.CheckUserAccessInterceptor;
 import com.donation.DonationWeb.interceptor.LoginCheckInterceptor;
 import com.donation.DonationWeb.interceptor.PostLoginCheckInterceptor;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import javax.servlet.Filter;
 import java.util.List;
 
 @Configuration
@@ -32,21 +38,27 @@ public class WebConfig implements WebMvcConfigurer {
 
 
     }
-
-
     //argumentResolvers등록
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
         resolvers.add(new LoginMemberIdArgumentResolver());
 
     }
-
-    //필터등록
+   //cros 메서드
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**").allowedOrigins("http://localhost:9090");
     }
 
+    @Bean
+    @Order(Ordered.HIGHEST_PRECEDENCE) //빈의 우선 순위 저장 우선순위 1순위
+    public FilterRegistrationBean crosFilter(){
+        FilterRegistrationBean<Filter> filterRegistrationBean = new FilterRegistrationBean<>();
+        filterRegistrationBean.setFilter(new CorsFilter());
+        filterRegistrationBean.setOrder(1);
+        filterRegistrationBean.addUrlPatterns("/*");
+        return filterRegistrationBean;
+    }
 
 }
 
